aa1d4c269a15806e030d5903fccf2a5835ce1dc2
ANJARAMILLOO <anjaramilloo@ucvvirtual.edu.pe>
2025-08-27 21:33:51 -0500

    Update api_modelo.py

--- api_modelo.py
+++ api_modelo.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
 import joblib
-import os
 
 app = Flask(__name__)
 
@@ -74,6 +73,3 @@ def predecir():
     except Exception as e:
         return jsonify({"error": f"Error al procesar datos: {str(e)}"}), 500
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000)) 
-    app.run(debug=True, host='0.0.0.0', port=port)
